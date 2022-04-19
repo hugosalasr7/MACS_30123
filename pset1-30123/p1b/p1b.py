598997a881dbc8c242c05f14ef8223001b182653
Hugo Salas R <hugo.salas.r@outlook.com>
2022-04-19 03:19:00 -0500

    created p1b

--- pset1-30123/p1b/p1b.py
+++ pset1-30123/p1b/p1b.py
@@ -3,24 +3,16 @@ import scipy.stats as sts
 from numba import jit
 import time
 from numba.pycc import CC
-
-cc = CC('pset1_30123')
-@cc.export('sim_loop_jit', 'f4, i4, i4, f8[:,:], f8[:,:], f4, f4')
-
-def sim_loop_jit(z_0, S, T, eps_mat, z_mat, rho, mu):
-    for s_ind in range(S):
-        z_tm1 = z_0
-        for t_ind in range(T):
-            e_t = eps_mat[t_ind, s_ind]
-            z_t = rho * z_tm1 + (1 - rho) * mu + e_t
-            z_mat[t_ind, s_ind] = z_t
-            z_tm1 = z_t
-
-cc.compile()
-
+from mpi4py import MPI
 import pset1_30123
 
-def simulate_health(rho = 0.5, mu = 3.0, sigma = 1.0, numba = False):
+def simulate_health(rho = 0.5, mu = 3.0, sigma = 1.0):
+
+    start = time.time() #Start timing
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
 
     # Set model parameters
     rho = rho
@@ -30,19 +22,24 @@ def simulate_health(rho = 0.5, mu = 3.0, sigma = 1.0, numba = False):
 
     # Set simulation parameters, draw all idiosyncratic random shocks,
     # and create empty containers
-    S = 1000 # Set the number of lives to simulate
+    S = int(1000/size) # Set the number of lives to simulate
     T = int(4160) # Set the number of periods for each simulation
-    np.random.seed(25)
+    np.random.seed(rank)
     eps_mat = sts.norm.rvs(loc=0, scale=sigma, size=(T, S))
     z_mat = np.zeros((T, S))
 
-    if numba:
-        pset1_30123.sim_loop_jit(z_0, S, T, eps_mat, z_mat, rho, mu)
-    else:
-        sim_loop(z_0, S, T, eps_mat, z_mat, rho, mu)
+    pset1_30123.sim_loop_jit(z_0, S, T, eps_mat, z_mat, rho, mu)
 
-#Without Numba
-start = time.time()
-simulate_health(numba = True)
-end = time.time()
-print("Numba: elapsed seconds = %s" % (end - start))
+    z_mat_all = None
+    if rank == 0:
+      z_mat_all = np.empty([T, S*size], dtype ='float')
+    
+    comm.Gather(sendbuf=z_mat, recvbuf=z_mat_all, root=0)
+
+    if rank == 0:
+      end = time.time() #End timing
+      print(f'Elapsed seconds with {size} cores: {end - start}')
+
+
+if __name__ == '__main__':
+    simulate_health()
